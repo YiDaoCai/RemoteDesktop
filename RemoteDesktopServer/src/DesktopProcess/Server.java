3075a0d4610f1d709c1a7fd0f0b6a405f1f4f259
lanjinglingiii <andy1996@126.com>
2016-10-07 00:38:10 +0800

    更新服务端线程以及聊天对话框

--- RemoteDesktopServer/src/DesktopProcess/Server.java
+++ RemoteDesktopServer/src/DesktopProcess/Server.java
@@ -1,13 +1,8 @@
 package DesktopProcess;
-import java.io.BufferedInputStream;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
+
 import java.io.IOException;
 import java.net.InetAddress;
 import java.net.ServerSocket;
-import java.net.Socket;
 
 
 public class Server {
@@ -15,7 +10,6 @@ public class Server {
 	private final String HostName;
 	private int port;
 	private ServerSocket server;
-	private Socket socket;
 	public Server() {
 		InetAddress ia=null;
         try {
@@ -40,81 +34,13 @@ public class Server {
 	/**
 	 * @return
 	 * 打开Socket通信
+	 * @throws IOException 
 	 */
-	public boolean openSocket() {
-		try {
-			socket = server.accept();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
+	public void openSocket() throws IOException {
+		while(true) {
+			ServerThread conn = new ServerThread(server.accept());
+			conn.start();
 		}
-		System.out.println("建立socket连接");
-		return true;
-	}
-	/**
-	 * @return
-	 * 关闭Socket通信
-	 */
-	public boolean closeSocket() {
-		try {
-			this.socket.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return false;
-		}
-		return true;
-	}
-	/**
-	 * @return
-	 * 向客户端发送消息
-	 */
-	public boolean send(String filepath) {
-		File file = new File(filepath);
-		//int flen = (int)file.length();
-		 //DataInputStream dis;
-		try {
-			//dis = new DataInputStream(new BufferedInputStream(socket.getInputStream()));
-			//dis.readByte();
-			DataInputStream fis = new DataInputStream(new BufferedInputStream(new FileInputStream(filepath)));
-            DataOutputStream ps = new DataOutputStream(socket.getOutputStream());
-            ps.writeUTF(file.getName());
-            ps.flush();
-            ps.writeLong((long) file.length());
-            ps.flush();
-
-            int bufferSize = 8192;
-            byte[] buf = new byte[bufferSize];
-
-            while (true) {
-                int read = 0;
-                if (fis != null) {
-                    read = fis.read(buf);
-                }
-
-                if (read <= 0) {
-                    break;
-                }
-                ps.write(buf, 0, read);
-            }
-            System.out.println("文件发送完成");
-            ps.flush();
-            ps.close();
-            fis.close();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-         
-		return false;
-	}
-	/**
-	 * @return
-	 * 接收来自客户端的消息
-	 */
-	public boolean receive() {
-		
-		return false;
 	}
 	public String getHostName() {
 		return HostName;
--- RemoteDesktopServer/src/DesktopUI/myinterface.java
+++ RemoteDesktopServer/src/DesktopUI/myinterface.java
@@ -1,58 +1,78 @@
 package DesktopUI;
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.Insets;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
 
+import javafx.scene.layout.Border;
+
+import javax.imageio.ImageIO;
+import javax.swing.Icon;
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.plaf.IconUIResource;
+import javax.swing.plaf.basic.BasicButtonUI;
+import javax.swing.text.IconView;
+
+import sun.awt.IconInfo;
 
 public class myinterface extends JFrame {
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	JButton btn1,btn2,btn3,btn4,btn5;
+	JButton setterBtn,prtScBtn,tranFileBtn,taskViewBtn,shutdown_upBtn;
 	FileFrame fileframe;
-	public myinterface() {
+	public myinterface() throws Exception {
 		super("YiDaoCai远程桌面监控");
-		fileframe = new FileFrame();
-		btn1=new JButton("设置");
-		btn2=new JButton("截屏");
-		btn3=new JButton("传输文件");
-		btn4=new JButton("进程监控");
-		btn5=new JButton("开关机");
-		Container cp=getContentPane();
-		cp.setLayout(null);
+		//fileframe = new FileFrame();
+		//setterBtn=new JButton("设置");
+		setterBtn = createBtn("设置", "./image/set.png");
+		prtScBtn=new JButton("截屏");
+		tranFileBtn=new JButton("传输文件");
+		taskViewBtn=new JButton("进程监控");
+		shutdown_upBtn=new JButton("开关机");
+		Container cp = getContentPane();
+		cp.setLayout(new BorderLayout());
 		
+		JPanel p = new JPanel(new GridLayout(1, 5));
 		//设置按钮监听
-		
-		btn3.addActionListener(new Action());
+		//p.preferredSize();
+		tranFileBtn.addActionListener(new Action());
 		
 		//设置按钮大小与边距
-		btn1.setBounds(50,50,100,50);
-		btn2.setBounds(200,50,100,50);
-		btn3.setBounds(350,50,140,50);
-		btn4.setBounds(540,50,140,50);
-		btn5.setBounds(730,50,120,50);
+		//setterBtn.setBounds(50,50,100,50);
+		//prtScBtn.setBounds(200,50,100,50);
+		//tranFileBtn.setBounds(350,50,140,50);
+		//taskViewBtn.setBounds(540,50,140,50);
+		//shutdown_upBtn.setBounds(730,50,120,50);
 		
 		//设置按钮字体与大小
-		btn1.setFont(new Font("宋体",Font.PLAIN,20));
-		btn2.setFont(new Font("宋体",Font.PLAIN,20));
-		btn3.setFont(new Font("宋体",Font.PLAIN,20));
-		btn4.setFont(new Font("宋体",Font.PLAIN,20));
-		btn5.setFont(new Font("宋体",Font.PLAIN,20));
+		setterBtn.setFont(new Font("宋体",Font.PLAIN,20));
+		prtScBtn.setFont(new Font("宋体",Font.PLAIN,20));
+		tranFileBtn.setFont(new Font("宋体",Font.PLAIN,20));
+		taskViewBtn.setFont(new Font("宋体",Font.PLAIN,20));
+		shutdown_upBtn.setFont(new Font("宋体",Font.PLAIN,20));
 		
-		cp.add(btn1);
-		cp.add(btn2);
-		cp.add(btn3);
-		cp.add(btn4);
-		cp.add(btn5);
+		p.add(setterBtn);
+		p.add(prtScBtn);
+		p.add(tranFileBtn);
+		p.add(taskViewBtn);
+		p.add(shutdown_upBtn);
+		
+		cp.add(p, BorderLayout.NORTH);
 	}
 	
 	public class Action implements ActionListener {
@@ -60,20 +80,34 @@ public class myinterface extends JFrame {
 		@Override
 		public void actionPerformed(ActionEvent e) {
 			// TODO Auto-generated method stub
-			if(e.getSource() == btn3) {
+			if(e.getSource() == tranFileBtn) {
 				fileframe.setVis(true);
 			}
 		}
 		
 	}
 	
-	public static void main(String[] args)
+	public static void main(String[] args) throws Exception
 	{
-		myinterface frame=new myinterface();
+		myinterface frame = new myinterface();
 		frame.setSize(1000,900);
 		frame.setLocation(500,40);
 		frame.setVisible(true);
 		frame.setBackground(Color.yellow);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
+	
+	private JButton createBtn(String text, String icon) {
+		ImageIcon ii = new ImageIcon(icon);
+		Image image = ii.getImage(); 
+		Image smallImage = image.getScaledInstance(64,64,Image.SCALE_FAST);
+		JButton btn = new JButton(text, new ImageIcon(smallImage));
+		btn.setUI(new BasicButtonUI());// 恢复基本视觉效果
+		btn.setPreferredSize(new Dimension(64, 64));// 设置按钮大小
+		btn.setContentAreaFilled(false);// 设置按钮透明
+		btn.setFont(new Font("粗体", Font.PLAIN, 15));// 按钮文本样式
+		btn.setMargin(new Insets(0, 0, 0, 0));// 按钮内容与边框距离
+		//btn.addMouseListener(new MyMouseListener(this));
+		return btn;
 	}
 }
